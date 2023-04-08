f84e1d21ba4b6d49b9a63ad6de85acba179f19e9
Mehradi <mehradi.info@gmail.com>
2023-04-08 23:37:50 +0200

    Adding ConfigModule & MongooseModule forRoot

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
