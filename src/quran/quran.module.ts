8a461d493002ee6c320769ecb89b33e9b56381ab
Mehradi <mehradi.info@gmail.com>
2023-04-09 16:45:25 +0200

    Importing MongooseModule in QuranModule

--- src/quran/quran.module.ts
+++ src/quran/quran.module.ts
@@ -2,7 +2,13 @@ import { Module } from '@nestjs/common';
 import { QuranService } from './quran.service';
 import { QuranResolver } from './quran.resolver';
 
+import { Quran, QuranSchema } from './schemas/quran.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+
 @Module({
-  providers: [QuranResolver, QuranService]
+  imports: [
+    MongooseModule.forFeature([{ name: Quran.name, schema: QuranSchema }]),
+  ],
+  providers: [QuranResolver, QuranService],
 })
 export class QuranModule {}
