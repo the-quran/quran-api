09bfbcf6ff9f4ecf2d570fe2383f59385902777f
Mehradi <mehradi.info@gmail.com>
2023-04-11 15:23:22 +0200

    #13 Creating props in ChapterSchema

--- src/chapter/schemas/chapter.schema.ts
+++ src/chapter/schemas/chapter.schema.ts
@@ -7,6 +7,20 @@ export type ChapterDocument = HydratedDocument<Chapter>;
 export class Chapter {
   @Prop()
   id: number;
+  @Prop()
+  revelation_place: string;
+  @Prop()
+  revelation_order: number;
+  @Prop()
+  bismillah_pre: boolean;
+  @Prop()
+  name_simple: string;
+  @Prop()
+  name_complex: string;
+  @Prop()
+  name_arabic: string;
+  @Prop()
+  verses_count: number;
 }
 
 export const ChapterSchema = SchemaFactory.createForClass(Chapter);
