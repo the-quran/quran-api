60e04af755802442dc3953aee698dd1e60396251
Sadiq Mehradi <mehradi.info@gmail.com>
2023-04-16 21:42:08 +0200

    Injecting chapterModel in chapterService

--- src/chapter/chapter.service.ts
+++ src/chapter/chapter.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateChapterInput } from './dto/create-chapter.input';
 import { UpdateChapterInput } from './dto/update-chapter.input';
+import { Chapter } from './schemas/chapter.schema';
 
 @Injectable()
 export class ChapterService {
+  constructor(
+    @InjectModel(Chapter.name) private readonly chapterModel: Model<Chapter>,
+  ) {}
   create(createChapterInput: CreateChapterInput) {
     return 'This action adds a new chapter';
   }
